645c69ef889ad89f187f992290c0f26d81ebe70e
Levi Zitting <levizitting5663@gmail.com>
2016-10-31 21:59:20 -0500

    Moved static HTML over to React

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Link } from "react-router";
 
-export default class Layout extends React.Component {
-	navigate() {
-		this.props.history.pushState(null, "/");
-	}
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
 
+export default class Layout extends React.Component {
 	render() {
+		const { location } = this.props;
+		const containerStyle = {
+			marginTop: "60px"
+		}
 		return (
 			<div>
-				<h1>KillerNews.net</h1>
-				{this.props.children}
-				<Link to="archives" class="btn btn-default">Archives</Link>
-				<Link to="settings" class="btn btn-default">Settings</Link>
-				<button class="btn btn-default" onClick={this.navigate.bind(this)}>Featured</button>
+
+				<Nav location={location} />
+				
+				<div class="container" style={containerStyle}>
+					<div class="row">
+						<div class="col-lg-12">
+							<h1>KillerNews.net</h1>
+
+							{this.props.children}	
+
+						</div>
+					</div>
+					<Footer />
+				</div>
 			</div>
 		);
 	}
